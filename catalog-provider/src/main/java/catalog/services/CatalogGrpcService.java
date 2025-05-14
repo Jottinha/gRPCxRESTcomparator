1aaa67fa67cb581014ec741cfc0c9ae0beda7d3e
Jottinha <joaovitorfps852@gmail.com>
2025-05-14 18:08:00 -0300

    feat: Adding spring tools for control call

--- catalog-provider/src/main/java/catalog/services/CatalogGrpcService.java
+++ catalog-provider/src/main/java/catalog/services/CatalogGrpcService.java
@@ -6,7 +6,9 @@ import catalog.grpc.CatalogServiceGrpc;
 import catalog.grpc.Product;
 import catalog.models.Catalog;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 
+@GrpcService
 public class CatalogGrpcService extends CatalogServiceGrpc.CatalogServiceImplBase {
 
     private final CatalogService catalogService = new CatalogService();
@@ -14,6 +16,7 @@ public class CatalogGrpcService extends CatalogServiceGrpc.CatalogServiceImplBas
     @Override
     public void getCatalog(CatalogRequest request, StreamObserver<CatalogResponse> responseObserver) {
         Catalog catalog = catalogService.getCatalogByClient(request.getClient());
+
         CatalogResponse.Builder responseBuilder = CatalogResponse.newBuilder()
                 .setId(catalog.getId()).setClient(catalog.getClient());
         catalog.getProducts().forEach(p -> responseBuilder.addProducts(
